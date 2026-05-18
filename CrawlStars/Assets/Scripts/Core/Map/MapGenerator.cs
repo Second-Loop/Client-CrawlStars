cb26b891cd3d669425606a42f3946185d9e118eb
SikPang <87380790+SikPang@users.noreply.github.com>
2026-05-18 18:06:04 +0900

    파일 기반 맵 제네레이팅

--- CrawlStars/Assets/Scripts/Core/Map/MapGenerator.cs
+++ CrawlStars/Assets/Scripts/Core/Map/MapGenerator.cs
@@ -1,8 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+using Utility;
+
 namespace Core.Map {
-    public class MapGenerator {
-        private static MapGenerator _instance;
-        public static MapGenerator Instance => _instance ??= new MapGenerator();
+    public class MapGenerator : MonoBehaviour {
+        [SerializeField] private Transform mapRoot;
+
+        private static MapGenerator instance;
+        public static MapGenerator Instance => instance;
         
-        
+        private List<Tile> loadedTiles = new List<Tile>();
+
+        public const float TileScale = 1.2f;
+
+        private void Awake() {
+            if (instance != null) {
+                Debug.LogError($"{nameof(MapGenerator)} Instance가 {name}에서 중복 초기화 시도되었습니다.");
+                return;
+            }
+
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start() {
+            Generate(MapLoader.MapType.A);
+        }
+
+        public void Generate(MapLoader.MapType mapType) {
+            var mapData = MapLoader.LoadMapFile(mapType);
+            if (mapData == null) return;
+
+            Vector2 startPos = new Vector2(
+                -TileScale * (mapData.width - 1) * 0.5f,
+                -TileScale * (mapData.height - 1) * 0.5f
+            );
+            for (int y = 0; y < mapData.height; ++y) {
+                for (int x = 0; x < mapData.width; ++x) {
+                    var obj = ObjectPooling.Instance.Get<Tile>("Tile", mapRoot);
+                    if (obj == null) return;
+
+                    var tileType = mapData.map[y][x] == 1 ? Tile.TileType.Wall : Tile.TileType.Ground;
+                    obj.Initialize(tileType, TileScale);
+                    obj.transform.localPosition = startPos + new Vector2(x, y) * TileScale;
+                    loadedTiles.Add(obj);
+                }
+            }
+        }
+
+        public void Clear() {
+            foreach (var tile in loadedTiles) {
+                ObjectPooling.Instance.TryAbandon("Tile", tile.gameObject);
+            }
+            loadedTiles.Clear();
+        }
     }
 }
