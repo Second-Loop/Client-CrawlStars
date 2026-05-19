6cf5bac27b6b18890ff63c00eebb7afd0cce1433
SikPang <87380790+SikPang@users.noreply.github.com>
2026-05-19 18:48:32 +0900

    PlayerManager 구현, GameManager 역할 부임

--- CrawlStars/Assets/Scripts/Core/Player/PlayerListener.cs
+++ CrawlStars/Assets/Scripts/Core/Player/PlayerListener.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using Utility;
 
 namespace Core.Player {
     public class PlayerListener : MonoBehaviour {
         public int Id { get; set; }
 
-        public void MoveTo(Vector2 pos) {
-            transform.position = pos;
+        public void MoveTo(Vector2 position) {
+            transform.position = position;
+        }
+        
+        public void LookAt(Vector2 direction) {
+            // transform.LookAtZAxisOnly(direction);
         }
 
-        public void Attack(Vector2 dir) {
+        public void Attack() {
             // attack to dir
         }
     }
--- CrawlStars/Assets/Scripts/Core/Simulator/Simulator.cs
+++ CrawlStars/Assets/Scripts/Core/Simulator/Simulator.cs
@@ -5,33 +5,56 @@ using UnityEngine;
 
 // 나중에 클라에서도 tick 을 계산해야 하기에 서버로 이주 후에는 이름을 TickManager로 바꾸기
 public class Simulator : MonoBehaviour {
-    [SerializeField] private InputProvider _inputProvider;
+    [SerializeField] private InputProvider inputProvider;
 
-    private float _accumulator = 0f;
+    public int TickCount { get; private set; }
+    private float accumulator;
+    private bool isStarted;
 
     private const int TickRate = 30;
     private const float TickThreshold = 1f / TickRate;
 
     private void Update() {
-        _accumulator += Time.deltaTime;
+        if (!isStarted) return;
 
-        if (_accumulator >= TickThreshold) {
+        accumulator += Time.deltaTime;
+
+        if (accumulator >= TickThreshold) {
             Tick();
-            _accumulator -= TickThreshold;
+            accumulator -= TickThreshold;
         }
     }
 
+    public void Initialize() {
+        accumulator = 0;
+        TickCount = 0;
+        isStarted = false;
+    }
+
+    public void StartTime() {
+        isStarted = true;
+    }
+    
+    // temp data
+    private Vector2 playerPos = Vector2.zero;
+    private Vector2 playerLookDir = Vector2.zero;
+    private const float MoveSpeed = 0.2f;
+
     private void Tick() {
         // Get player's input
-        Vector2 moveDirection = _inputProvider.GetMoveDirection();
-        Vector2 lookDirection = _inputProvider.GetLookingSide();
-        bool isPressedAttack = _inputProvider.GetAttack();
+        Vector2 moveDirection = inputProvider.GetMoveDirection();
+        Vector2 lookDirection = inputProvider.GetLookingSide();
+        bool isPressedAttack = inputProvider.GetAttack();
 
         // Simulate
+        playerPos += moveDirection * MoveSpeed;
+        playerLookDir += lookDirection;
         
         // Return to clients
-        PlayerManager.Instance.Move();
-        PlayerManager.Instance.Look();
-        PlayerManager.Instance.Attack();
+        PlayerManager.Instance.Move(playerPos);
+        PlayerManager.Instance.Look(playerLookDir);
+        if (isPressedAttack) {
+            PlayerManager.Instance.Attack();
+        }
     }
 }
